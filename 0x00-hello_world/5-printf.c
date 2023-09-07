43cc4042cfb1e387e1a4a3f04d0feac7fa5d275f
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-09-07 07:59:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - Prints a line using printf function
+ * Return: Returns 0
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
