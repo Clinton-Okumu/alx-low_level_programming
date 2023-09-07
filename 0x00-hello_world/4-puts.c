b1e4c65da41a0183133fd3275f8b4e3fd8d6d993
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-09-07 07:05:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ *main - Prints Programming is like building a multilingual puzzle
+ *Return: Returns 0 indicates successful execution
+ */
+
+
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
