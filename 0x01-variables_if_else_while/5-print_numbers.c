72bf208cf715d7430e270415ad9275f925c5b9a0
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-09-08 10:34:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function to print base 10 numbers
+ * Return: return (0) success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+{
+	printf("%d\n", i);
+}
+return (0);
+}
