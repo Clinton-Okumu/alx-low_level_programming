a0f56e43d06822ed547a2edd4cd6d126bf1ad96b
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-09-29 10:12:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - program that prints its name, followed by a new line.
+ * @argv: array
+ *@argc: number of arguments
+ * Return: (0) success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_HEDER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
