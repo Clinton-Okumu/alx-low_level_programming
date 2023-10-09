aa690cda5617edd534c42d1014572ecbf40af307
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-10-09 07:54:04 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ *init_dog - function that initialize a variable of type struct dog
+ *@d: Pointer to the struct dog to be initialized
+ *@name: Pointer to a string for the dog's name
+ *@age: Age of the dog as a float
+ *@owner: Pointer to a string for the owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
