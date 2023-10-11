0b59e07897c0acf9bdae9f6182efe0a210f7f7b8
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-10-11 04:38:31 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - Prints a name using a provided function.
  * @name: The name to be printed.
  * @f: A function that will be used to print the name.
+ *Rerurn: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
