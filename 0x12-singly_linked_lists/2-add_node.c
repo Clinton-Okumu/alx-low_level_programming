2f46c10f457aa12735dcd7324fed1dd7c51ce409
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-10-19 08:05:28 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,8 @@
 #include "lists.h"
 /**
  *add_node - function that adds a new node
- *@str - string
- *@head - head node
+ *@str: string
+ *@head: head node
  *Return: (new_node)
  */
 list_t *add_node(list_t **head, const char *str)
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
