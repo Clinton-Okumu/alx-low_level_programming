90f79ac7bff5acad99d12e10034a6d131c2386a2
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-10-23 01:43:55 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_listint - function that prints all the elements of a listint_t
+ *@h: pointer
+ *Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n",  h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+int _putchar(char c);
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
