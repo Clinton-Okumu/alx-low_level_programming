4412667ecb1a98e932bce19d207d5772c275fc20
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-10-24 08:25:52 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *sum_listint - function that returns the sum of all the data
+ *@head: pointer
+ *Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
